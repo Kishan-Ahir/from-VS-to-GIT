2af1693aca21284a20d57cb0bd07487d573b1b8b
Kishan Chandravadiya <123851397+Kishan-Ahir@users.noreply.github.com>
2023-05-20 13:35:24 +0530

    queryselector task

---
+++ queryselector.js
@@ -0,0 +1,7 @@
+
+let second_item = document.querySelector(".list-group-item:nth-child(2)");
+second_item.style.backgroundColor = "green";
+
+let third_item = document.querySelector(".list-group-item:nth-child(3)");
+third_item.style.display = "none";
+
