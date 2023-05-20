579a0e5f7d5b1c890840963c8bb7aee96cbc6e7d
Kishan-Ahir <chandravadiyakishan77@gmail.com>
2023-05-20 15:23:16 +0530

    creatingNodesandModifyingDom

--- Lets get started with DOM/CreatingNodesandModifyingDom.js
+++ Lets get started with DOM/CreatingNodesandModifyingDom.js
@@ -17,3 +17,15 @@ new_div.setAttribute("title","title");
 let new_text_node = document.createTextNode("newtextnode");
 new_div.appendChild(new_text_node);
 console.log(new_div);
+
+let div = document.querySelector("#selecteddiv");
+let head = document.querySelector("header-title");
+let newdiv = document.createElement("div");
+let newtextnode = document.createTextNode("HEllo word");
+newdiv.appendChild(newtextnode);
+div.insertBefore(newdiv,head);
+
+let newdiv2 = document.createElement("div");
+let newtextnode2 = document.createTextNode("HEllo word");
+newdiv2.appendChild(newtextnode2);
+ul.insertBefore(newdiv2,li);
