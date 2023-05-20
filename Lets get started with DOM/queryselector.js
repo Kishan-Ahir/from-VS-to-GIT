8afa805cce2963e8040685a7319d000dffbf3061
Kishan-Ahir <chandravadiyakishan77@gmail.com>
2023-05-20 11:45:44 +0530

    queryselector

--- Lets get started with DOM/queryselector.js
+++ Lets get started with DOM/queryselector.js
@@ -3,3 +3,6 @@ li.innerText = "Hello Kishan";
 
 let third_item = document.querySelector(".list-group-item:nth-child(3)");
 third_item.style.color = "red";
+
+let last = document.querySelector(".list-group-item:last-child");
+last.style.color = "green";
