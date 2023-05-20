a0f70cb7d79ac5278a35c78bb6ea7465ce8c2eb8
Kishan-Ahir <chandravadiyakishan77@gmail.com>
2023-05-20 13:13:52 +0530

    queryselector task

--- Lets get started with DOM/queryselector.js
+++ Lets get started with DOM/queryselector.js
@@ -1,6 +1,6 @@
 
 let second_item = document.querySelector(".list-group-item:nth-child(2)");
-second_item.style.color = "green";
+second_item.style.backgroundColor = "green";
 
 let third_item = document.querySelector(".list-group-item:nth-child(3)");
 third_item.style.display = "none";
